6125304a18ca07a52915e777fb6577b75d113544
Adebayo Gbadebo <gbadeboadebayo1000@gmail.com>
2026-03-06 16:59:30 +0100

    fix: update next-auth type definitions for custom roles

--- src/next-auth.d.ts
+++ src/next-auth.d.ts
@@ -3,20 +3,27 @@ import { JWT } from "next-auth/jwt"
 
 declare module "next-auth" {
     interface User {
+        role?: string
         workspaceId?: string
         lawyerToken?: string
+        isPlatformAdmin?: boolean
     }
     interface Session {
         user: {
             id: string
+            role: string
             workspaceId: string
             lawyerToken: string
+            isPlatformAdmin: boolean
         } & DefaultSession["user"]
     }
 }
 
 declare module "next-auth/jwt" {
     interface JWT {
+        role?: string
         workspaceId?: string
+        lawyerToken?: string
+        isPlatformAdmin?: boolean
     }
 }
